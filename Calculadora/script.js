1dea89000291d0fc676237c2795062164df53362
jayler0203 <jayler0203@gmail.com>
2023-10-20 20:53:30 -0500

    primer commit

---
+++ Calculadora/script.js
@@ -0,0 +1,26 @@
+let currentInput="";
+let display = document.getElementById("display")
+function appendToDisplay(value) {
+    currentInput += value;
+    display.value = currentInput    
+}
+function clearDisplay(){
+    currentInput = "";
+    display.value = "";
+}
+
+function calculatedResult() {
+    try{
+        const result = eval(currentInput)
+        display.value = result;
+        currentInput = result.toString()
+    }catch(error){
+        display.value = `${error}`
+
+    }
+    
+}
+
+
+
+
